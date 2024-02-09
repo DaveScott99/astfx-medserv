3acd419006b9943f20db8699a77b07a7d705a300
DaviScott99 <davicalixto2077@gmail.com>
2024-02-08 23:03:31 -0300

    feat: create findMovieById
    Signed-off-by: DaviScott99 <davicalixto2077@gmail.com>

--- src/main/java/com/daverj/media/controller/MovieController.java
+++ src/main/java/com/daverj/media/controller/MovieController.java
@@ -6,6 +6,7 @@ import com.daverj.media.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,9 +21,14 @@ public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Iterable<MediaDTO>> list(Pageable pageable) {
+    @GetMapping("/all")
+    public ResponseEntity<Page<MediaDTO>> list(Pageable pageable) {
         return new ResponseEntity<>(movieService.list(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<MediaDTO> findById(@RequestParam Long id) {
+        return ResponseEntity.ok(movieService.findById(id));
     }
 
     @PostMapping
--- src/main/java/com/daverj/media/service/MovieService.java
+++ src/main/java/com/daverj/media/service/MovieService.java
@@ -4,12 +4,13 @@ import com.daverj.media.dto.request.MediaCreateDTO;
 import com.daverj.media.dto.response.MediaDTO;
 import com.daverj.media.dto.mapper.MediaMapper;
 import com.daverj.media.repository.MovieRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 
 @Service
 @Slf4j
@@ -19,10 +20,16 @@ public class MovieService {
     private final MovieRepository movieRepository;
     private final MediaMapper mediaMapper;
 
-    public Iterable<MediaDTO> list(Pageable pageable) {
+    public Page<MediaDTO> list(Pageable pageable) {
         log.info("Listing all movies");
         return movieRepository.findAll(pageable)
                 .map(mediaMapper::toMovieDTO);
+    }
+
+    public MediaDTO findById(Long id) {
+        return movieRepository.findById(id)
+                .map(mediaMapper::toMovieDTO)
+                .orElseThrow(() -> new EntityNotFoundException("Movie not found"));
     }
 
     public MediaDTO create(MediaCreateDTO movie) {
